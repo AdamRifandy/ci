3f87e51bfd56a828f08e4fa1cd60b47af2aff084
AdamRifandy <adamrifandy123@gmail.com>
2026-05-19 09:04:56 +0700

    Updated application/views/template_sederhana/v_header.php

--- application/views/template_sederhana/v_header.php
+++ application/views/template_sederhana/v_header.php
@@ -20,6 +20,7 @@
                     <li><a href="<?php echo base_url() . 'index.php/web'
                                     ?>">Home</a></li>
                     <li><a href="<?php echo base_url() . 'index.php/web/about' ?>">About</a></li>
+                    <li><a href="<?php echo base_url() . 'index.php/web/profile' ?>">Profile</a></li>
                 </ul>
             </nav>
             <div class="clear"></div>
