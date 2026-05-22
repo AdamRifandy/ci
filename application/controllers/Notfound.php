455b8f319f1486ee6e0baf6c502ce597cb2d54a6
Nothing... <adamrifandy123@gmail.com>
2026-05-22 04:56:31 -0700

    Change view name from notfound_page to v_notfound

--- application/controllers/Notfound.php
+++ application/controllers/Notfound.php
@@ -3,6 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Notfound extends CI_Controller {
 	public function index() {
-		$this->load->view('notfound_page');
+		$this->load->view('v_notfound');
 	}
 }
